28a97c420e0e02e7382faa0ea31fc55fbab456bb
JonPro0 <zellerhoff05@gmail.com>
2021-12-07 10:35:13 +0100

    Add new Gui

---
+++ src/gui/Wettkampf/WettkampfGUI.java
@@ -0,0 +1,38 @@
+package gui.Wettkampf;
+
+import gui.ArrayPanel;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class WettkampfGUI extends JFrame {
+    private JPanel pWMain;
+    private JPanel pBubble;
+    private JPanel pInsertion;
+    private JPanel pSelection;
+    private JPanel pBogo;
+    private JTextArea bogoSortTextArea;
+    private JTextArea selectionSortTextArea;
+    private JTextArea insertionSortTextArea;
+    private JTextArea bubbleSortTextArea;
+
+    public WettkampfGUI() {
+        int[] array = {3, 5,12, 5, 8, 78, 8};
+        BubblearrayPanel bArray = new BubblearrayPanel(array);
+        pBubble.add(new BubblearrayPanel(array));
+        pInsertion.add(new BubblearrayPanel(array));
+        pSelection.add(new BubblearrayPanel(array));
+        pBogo.add(new BubblearrayPanel(array));
+
+
+        add(pWMain);
+        setSize(800, 600);
+        setTitle("Mein Fenster 11G4");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new WettkampfGUI();
+    }
+}
